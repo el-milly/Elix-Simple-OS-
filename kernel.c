856d7843e365669a0519ea31dba5408a42145c44
el-milly <elijahhome.job@gmail.com>
2025-11-22 14:46:19 +0300

    linker script

--- kernel.c
+++ kernel.c
@@ -33,7 +33,7 @@ static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
     return fg | bg << 4;
 }
 
-static inline uint16_t vga_entry(unsigned char uc, color)
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
     return (uint16_t) uc | (uint16_t) color << 8;
 }
@@ -70,7 +70,7 @@ void terminal_initialize(void)
 
 void terminal_setcolor(uint8_t color)
 {
-    terminal_color = color
+    terminal_color = color;
 }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
@@ -86,7 +86,7 @@ void terminal_putchar(char c)
     if(++terminal_column==VGA_WIDTH) {
         terminal_column = 0;
         if(++terminal_row == VGA_HEIGHT)
-            terminal_row = 0
+            terminal_row = 0;
     }
 }
 
